002f830c27e92e1c661aa20a9a4e021f8b3db086
Jani Sourander <sourander@gmail.com>
2019-06-30 10:57:06 +0300

    hdmi.drawimg added

--- wambilight/hdmi.py
+++ wambilight/hdmi.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import imutils
 
 class Hdmi:
     """This creates a PyGame Surface for displaying
@@ -16,11 +17,32 @@ class Hdmi:
         print(pygame.display.Info())
         self.w = pygame.display.Info().current_w 
         self.h = pygame.display.Info().current_h 
-        self.lcd = pygame.display.set_mode((self.w,self.h), pygame.FULLSCREEN)
+        self.lcd = pygame.display.set_mode((self.w,self.h), pygame.FULLSCREEN, 24)
     
     def fill(self, r,g,b):
         self.lcd.fill((r,g,b))
-        pygame.display.update()
+        pygame.display.flip()
     
     def update(self):
-        pygame.display.update()
+        pygame.display.flip()
+        
+    def drawimg(self, arrayimg):
+        """OpenCV and NumPy use (h, w, c) ordering.
+        This method will scale down image and swap 
+        axis for PyGame surfarray"""
+        
+        # Fit the image to PyGame Surface bbox        
+        (height, width) = arrayimg.shape[:2]
+        if ( (width/height) >= (self.w/self.h)):
+            resized = imutils.resize(arrayimg, width=self.w)
+        else:
+            resized = imutils.resize(arrayimg, width=self.h)
+        
+        # Degub check
+        print(resized.shape)
+        
+        # Draw swapped axis
+        pygame.surfarray.blit_array(self.lcd, arrayimg.swapaxes(0,1))
+        pygame.display.flip()
+
+        
--- webcambilight.py
+++ webcambilight.py
@@ -5,3 +5,11 @@ from wambilight import Hdmi
 
 # Instansiate a PyGame Surface
 hdmi = Hdmi()
+
+
+image1 = cv2.imread("images/tv-test-1.png")
+image2 = cv2.imread("images/tv-test-1.png")
+image3 = cv2.imread("images/tv-test-1.png")
+
+
+
