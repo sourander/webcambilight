c6257c4705dc4e5d159bbc682494c5453c2868f7
Jani Sourander <sourander@gmail.com>
2019-07-01 11:30:53 +0300

    Find countours and 4pointTransform

--- wambilight/hdmi.py
+++ wambilight/hdmi.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import imutils
 import numpy as np
+import cv2
 
 class Hdmi:
     """This creates a PyGame Surface for displaying
@@ -36,37 +37,41 @@ class Hdmi:
         PyGame Surface (self.lcd) """
         
         diff = lcd_dim - image.shape[direction]
+        print("Upscaled size doesn't match the screen.\nOld size: {}".format(image.shape[:2]))
         
         # Let's remove some cols or rows
         if(diff > 0):
+            print("Adding col/row. Diff: {}, Axis:{}".format(diff, direction))
             if(direction==0):
-                print("Debug code 1")
-                new_col = np.zeros((diff, self.w, 3), dtype="uint8")
-                image = np.concatenate((image.copy(), new_col), direction)
+                new_row = np.zeros((diff, self.w, 3), dtype="uint8")
+                image = np.concatenate((image.copy(), new_row), direction)
             else:
-                print("Debug code 2")
                 new_col = np.zeros((self.h, diff, 3), dtype="uint8")
                 image = np.concatenate((image.copy(), new_col), direction)
         
         # ...or maybe we want to remove cols or rows
         elif(diff < 0):
-            print("Debug code 3")
+            print("Remove col/row. Diff: {}, Axis: {}".format(diff, direction))
             diff = abs(diff)
             image = np.delete(image, (range(0, diff)), axis=direction)
         
-        print("New size")
-        print(image.shape)
-        
+        print("New size: {}".format(image.shape[:2]))
+       
         return image    
           
           
     def drawimg(self, arrayimg):
         """OpenCV and NumPy use (h, w, c) ordering.
         This method will scale down image and swap 
-        axis for PyGame surfarray"""
+        axis for PyGame surfarray. """
+        
+        # Convert grayscale image to colour
+        if(arrayimg.ndim < 3):
+            arrayimg = cv2.cvtColor(arrayimg,cv2.COLOR_GRAY2RGB)
     
         (height, width) = arrayimg.shape[:2]
         
+        # Compare aspect ratio to lcd aspect ratio.
         if( (width/height) >= (self.w/self.h)):
             resized = imutils.resize(arrayimg, width=self.w) 
             if(resized.shape[0] != self.h):
