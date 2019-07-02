Bug fix in cornerpoint scaling

wambilight/ledupdater.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 from imutils.perspective import four_point_transform
 from timeit import default_timer as timer
 
@@ -7,6 +8,8 @@ from timeit import default_timer as timer
 class Ledupdater:
     def __init__(self, cornerpoints):
         self.pts = cornerpoints
+        self.vertleds = 60
+        self.horleds = 30
        
 
     def warp_and_draw(self, image, hdmi):
@@ -15,11 +18,23 @@ class Ledupdater:
 
         warped = four_point_transform(image, self.pts)
 
+        resized = cv2.resize(warped, (self.vertleds, self.horleds), 
+                             interpolation = cv2.INTER_CUBIC) 
+        
+        # Grab edge pixels into an array
+        top = resized[0,:]
+        right = resized[1:,-1]
+        bottom = np.flip(resized[-1,1:-1])
+        left = np.flip(resized[1:,0])
+        edgepixels = np.concatenate((top, right, bottom, left), axis=0)
+        
         # TIMER
         end = timer()
-        print((end - start)*1000)
+        print(round((end - start)*1000, 2), "milliseconds")
         
-        hdmi.drawimg(warped)
+        hdmi.drawimg(resized)
+        
+
 
 """
 FIRST METHOD OF GETTING EDGE PIXELS
@@ -31,10 +46,6 @@ image = np.array([[ 0,  1,  2,  3,  4,  5],
                   [24, 25, 26, 27, 28, 29],
                   [30, 31, 32, 33, 34, 35]])
 
-left = np.flip(image[1:,0])
-top = image[0,:]
-right = image[1:,-1]
-bottom = np.flip(image[-1,1:-1])
 
-edgepixels = np.concatenate((top, right, bottom, left), axis=0)
+
 """